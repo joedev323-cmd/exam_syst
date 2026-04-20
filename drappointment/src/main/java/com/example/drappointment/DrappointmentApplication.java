78ac410deb3f636aa50366b8bc6adfbaf9af28b4
joe <joedev.323@gmail.com>
2026-04-20 21:18:08 +0300

    maven

---
+++ drappointment/src/main/java/com/example/drappointment/DrappointmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.drappointment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrappointmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrappointmentApplication.class, args);
+	}
+
+}
---
+++ drappointment/src/test/java/com/example/drappointment/DrappointmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.drappointment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrappointmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
