29b264bcab655f47d238b13c86b5b0d3890b23a9
joe <joedev.323@gmail.com>
2026-04-20 22:40:50 +0300

    .

---
+++ dr_application/src/test/java/com/example/dr_application/DrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dr_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
